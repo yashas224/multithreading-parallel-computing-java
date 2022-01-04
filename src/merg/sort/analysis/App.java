1c6435fde826333566421729d4b0b38cfa08d166
yashas224 <71977329+yashas224@users.noreply.github.com>
2022-01-04 21:19:42 +0530

    Update App.java

--- src/merg/sort/analysis/App.java
+++ src/merg/sort/analysis/App.java
@@ -5,11 +5,9 @@ import java.util.Scanner;
 public class App {
 
 	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-		int size = scanner.nextInt();
-		int arr[] = new int[size];
-		for (int i = 0; i < size; i++) {
-			arr[i] = scanner.nextInt();
+		int arr[] = new int[101];
+		for (int i = 100; i >= 0; i--) {
+			arr[100 - i] = i;
 		}
 		MergeSort obj = new MergeSort(arr);
 		System.out.println("Before Sorting");
@@ -30,53 +28,56 @@ class MergeSort {
 	}
 
 	public int[] sort() {
-		divideAndMerge(0, nums.length - 1, nums);
+		divide(0, nums.length - 1, nums);
 		return nums;
 	}
 
-	private void divideAndMerge(int i, int j, int[] nums) {
+	private void divide(int i, int j, int[] nums) {
 		if (i < j) {
 
 			int m = (i + j) / 2;
-			divideAndMerge(i, m, nums);
-			divideAndMerge(m + 1, j, nums);
-
-			int[] partA = new int[m - i + 1];
-			int[] partB = new int[j - m];
-			int k = 0;
-			int l = 0;
-			for (l = i; l <= m; l++) {
-				partA[k++] = nums[l];
-			}
-			k = 0;
-			for (l = m + 1; l <= j; l++) {
-				partB[k++] = nums[l];
-			}
-
-			k = 0;
-			l = 0;
-			int start = i;
-			while (k < partA.length && l < partB.length) {
-				int a = partA[k];
-				int b = partB[l];
-				if (a < b) {
-					nums[start++] = a;
-					k++;
-				} else {
-					nums[start++] = b;
-					l++;
-				}
-			}
-
-			while (k < partA.length) {
-				nums[start++] = partA[k++];
-			}
-			while (l < partB.length) {
-				nums[start++] = partB[l++];
-			}
+			divide(i, m, nums);
+			divide(m + 1, j, nums);
+			mergeIntervals(i, j, nums, m);
 
 		}
 
+	}
+
+	private void mergeIntervals(int i, int j, int[] nums, int m) {
+		int[] partA = new int[m - i + 1];
+		int[] partB = new int[j - m];
+		int k = 0;
+		int l = 0;
+		for (l = i; l <= m; l++) {
+			partA[k++] = nums[l];
+		}
+		k = 0;
+		for (l = m + 1; l <= j; l++) {
+			partB[k++] = nums[l];
+		}
+
+		k = 0;
+		l = 0;
+		int start = i;
+		while (k < partA.length && l < partB.length) {
+			int a = partA[k];
+			int b = partB[l];
+			if (a < b) {
+				nums[start++] = a;
+				k++;
+			} else {
+				nums[start++] = b;
+				l++;
+			}
+		}
+
+		while (k < partA.length) {
+			nums[start++] = partA[k++];
+		}
+		while (l < partB.length) {
+			nums[start++] = partB[l++];
+		}
 	}
 
 	public void display() {
